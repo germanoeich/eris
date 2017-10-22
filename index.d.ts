88dcdd6bd7a9a24a415e58ae6e84ff26972d5e29
Brian Tanner <brian@tanner.io>
2017-10-22 00:30:23 -0400

    fix guild bug in category channel, typing fix

--- index.d.ts
+++ index.d.ts
@@ -363,7 +363,7 @@ declare module "eris" {
     ownerID?: string;
     splash?: string;
   }
-  interface MemberOptions { roles?: string[]; nick?: string; mute?: boolean; deaf?: boolean; channelID?: string; }
+  interface MemberOptions { roles?: string[]; nick?: string; mute?: boolean; deaf?: boolean; channelID?: string; reason?: string; }
   interface RoleOptions { name?: string; permissions?: number; color?: number; hoist?: boolean; mentionable?: boolean; }
   interface GamePresence { name: string; type?: number; url?: string; }
   interface SearchOptions {
